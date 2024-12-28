5e772254b23131c6806531cf47b9eea0f4ff81cb
AASani29 <alfey2001sani@gmail.com>
2024-12-28 06:42:44 +0600

    Navbar updated | All the services can be accessed from navbar now

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -5,14 +5,19 @@ import { useState } from "react";
 export default function Header() {
   const { currentUser } = useSelector((state) => state.user);
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+  const [isServicesDropdownOpen, setIsServicesDropdownOpen] = useState(false);
 
   const handleDropdownToggle = () => {
     setIsDropdownOpen(!isDropdownOpen);
   };
 
+  const handleServicesDropdownToggle = () => {
+    setIsServicesDropdownOpen(!isServicesDropdownOpen);
+  };
+
   return (
-    <header className="bg-white text-teal-700  w-full">
-      <div className="container mx-auto px-4 flex justify-between items-center py-4 ">
+    <header className="bg-white text-teal-700 w-full">
+      <div className="container mx-auto px-4 flex justify-between items-center py-4">
         {/* Logo */}
         <div className="flex items-center ml-10">
           <Link to="/home">
@@ -31,18 +36,13 @@ export default function Header() {
             </Link>
           )}
           {currentUser && (
-            
-              <Link
-                to="/analytics"
-                
-                className="text-teal-700 hover:text-teal-500 transition duration-300 font-semibold mt-2"
-              >
-                Analytics
-              </Link>
-              
-            
+            <Link
+              to="/analytics"
+              className="text-teal-700 hover:text-teal-500 transition duration-300 font-semibold mt-2"
+            >
+              Analytics
+            </Link>
           )}
-          
           {currentUser && (
             <Link
               to="/about"
@@ -51,6 +51,112 @@ export default function Header() {
               About
             </Link>
           )}
+
+          {/* Services Dropdown */}
+          <div className="relative">
+            <button
+              onClick={handleServicesDropdownToggle}
+              className="text-teal-700 hover:text-teal-500 transition duration-300 font-semibold mt-2"
+            >
+              Services
+            </button>
+            {isServicesDropdownOpen && (
+              <div className="absolute mt-2 bg-white border border-teal-300 rounded shadow-lg">
+                <ul className="py-2">
+                  <li>
+                    <Link
+                      to="/breathinggame"
+                      className="block px-4 py-2 text-teal-700 hover:bg-teal-100"
+                    >
+                      Breathing Game
+                    </Link>
+                  </li>
+                  <li>
+                    <Link
+                      to="/chatbot"
+                      className="block px-4 py-2 text-teal-700 hover:bg-teal-100"
+                    >
+                      Chatbot
+                    </Link>
+                  </li>
+                  <li>
+                    <Link
+                      to="/Mood-Tracker"
+                      className="block px-4 py-2 text-teal-700 hover:bg-teal-100"
+                    >
+                      Mood Tracker
+                    </Link>
+                  </li>
+                  <li>
+                    <Link
+                      to="/Sleep-Tracker"
+                      className="block px-4 py-2 text-teal-700 hover:bg-teal-100"
+                    >
+                      Sleep Tracker
+                    </Link>
+                  </li>
+                  <li>
+                    <Link
+                      to="/meditation"
+                      className="block px-4 py-2 text-teal-700 hover:bg-teal-100"
+                    >
+                      Meditation
+                    </Link>
+                  </li>
+                  <li>
+                    <Link
+                      to="/subscribe"
+                      className="block px-4 py-2 text-teal-700 hover:bg-teal-100"
+                    >
+                      Subscribe
+                    </Link>
+                  </li>
+                  <li>
+                    <Link
+                      to="/daily-journal"
+                      className="block px-4 py-2 text-teal-700 hover:bg-teal-100"
+                    >
+                      Daily Journal
+                    </Link>
+                  </li>
+                  <li>
+                    <Link
+                      to="/specialist"
+                      className="block px-4 py-2 text-teal-700 hover:bg-teal-100"
+                    >
+                      Specialist
+                    </Link>
+                  </li>
+                  
+                  <li>
+                    <Link
+                      to="/jokeoftheday"
+                      className="block px-4 py-2 text-teal-700 hover:bg-teal-100"
+                    >
+                      Joke of the Day
+                    </Link>
+                  </li>
+                  <li>
+                    <Link
+                      to="/musicrecommendation"
+                      className="block px-4 py-2 text-teal-700 hover:bg-teal-100"
+                    >
+                      Music Recommendation
+                    </Link>
+                  </li>
+                  <li>
+                    <Link
+                      to="/contents"
+                      className="block px-4 py-2 text-teal-700 hover:bg-teal-100"
+                    >
+                      Contents
+                    </Link>
+                  </li>
+                </ul>
+              </div>
+            )}
+          </div>
+
           <Link to="/profile" className="flex items-center space-x-2 font-bold">
             {currentUser ? (
               <img
