7f1ad891f394fa36cc713730fab3d7f64a507146
Faiza Maliat <faiza.maliat20@gmail.com>
2025-01-02 00:41:22 +0600

    Memory Game

--- client/src/pages/BreathingGame.jsx
+++ client/src/pages/BreathingGame.jsx
@@ -310,7 +310,7 @@ const MemoryGame = () => {
       </div>
       <button
         onClick={resetGame}
-        className="mt-4 px-4 py-2 bg-teal-600 text-white rounded hover:bg-teal-700"
+        className="mt-5 px-9 py-2 bg-teal-600 text-white rounded hover:bg-teal-700"
       >
         Reset Game
       </button>
