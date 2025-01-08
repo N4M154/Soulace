4db6cfe796ad5da8310379eb32f5b4576722beb1
Namisa Najah <namisa.najah.raisa@gmail.com>
2025-01-08 16:42:12 +0600

    maybe?

--- client/src/components/OAuth.jsx
+++ client/src/components/OAuth.jsx
@@ -13,20 +13,17 @@ export default function OAuth() {
       const auth = getAuth(app);
 
       const result = await signInWithPopup(auth, provider);
-      const res = await fetch(
-        "https://soulace-backend.onrender.com/api/auth/google",
-        {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({
-            name: result.user.displayName,
-            email: result.user.email,
-            photo: result.user.photoURL,
-          }),
-        }
-      );
+      const res = await fetch("https://soualce.vercel.app/api/auth/google", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          name: result.user.displayName,
+          email: result.user.email,
+          photo: result.user.photoURL,
+        }),
+      });
       const data = await res.json();
       console.log(data);
       dispatch(signInSuccess(data));
