fa2639c83cbde89f8d88d7af2f20f1b29f93385c
AASani29 <alfey2001sani@gmail.com>
2025-01-08 15:08:08 +0600

    url updated

--- api/index.js
+++ api/index.js
@@ -16,7 +16,7 @@ dotenv.config();
 // Enable CORS for frontend access
 const app = express();
 app.use(cors({
-  origin: "https://soulace.vercel.app/", // Frontend origin
+  origin: "https://soulace.vercel.app", // Frontend origin
   credentials: true, // Allow credentials (cookies)
 }));
 
